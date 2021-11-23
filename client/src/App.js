b89494f67783a69020a7a2d195eb7386bec6dc11
daharrington60 <90582954+daharrington60@users.noreply.github.com>
2021-11-23 17:28:03 -0500

    more formatting (#9)

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,12 @@ const App = () => {
     <AuthProvider>
     <Router>
     <div className="App">
-      <NavBar />
+     <div className="body_bars ml-0 mr-0 pl-0 pr-0">
+        <div className="row col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mx-10">
+	    <NavBar />
+	 </div>
+      </div>
+      <div className="body_bars ml-2 mr-0 pl-0 pr-0">
 	    {/* only act on one route  */}
 	       <Switch>
                  <Route exact path="/" component={Home} />
@@ -47,6 +52,7 @@ const App = () => {
 	         {/* all other routes will default to the 404 error page */}
                  <Route component={ErrorPage} />
 	       </Switch>
+    </div>
     </div>
     </Router>
     </AuthProvider>
