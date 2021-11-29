86aa5132c543307c72c3759781c22bbca0e22467
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2021-11-29 00:47:49 -0500

    Updated seed file to generate in a bound box near NJ

--- server/src/tasks/seed.js
+++ server/src/tasks/seed.js
@@ -1,16 +1,16 @@
 const { geocode } = require("../config/mongoCollections");
 const mongoConnection = require("../config/mongoConnection");
 const { ObjectId } = require("mongodb");
-// #MARK:- Data construction operations, does *not* do validation, that is handled in data functions
+
+// #MARK:- Data construction operations, does *not* do validation, that is handled in data functions (or programmer validation when bypassed)
 
 const createGeocodedPoint = (longitude, latitude) => {
     return {
-        // longitude: longitude,
-        // latitude: latitude,
         coordinates: [longitude, latitude],
         type: "Point"
     }
 }
+
 
 
 
@@ -24,14 +24,28 @@ async function seedDB() {
     await geocodeCollection.deleteMany({});
 
 
+    /// Arbitrarily drawn bounding box which contains all of NJ, and parts of NY, PA, DE, MD
+    const westLong = -76.223145
+    const eastLong = -73.828125
+    const southLat = 38.908133
+    const northLat = 43.317185
+
+
     try {
         console.log("Inserting points")
         const points = []
-        for(let i = 0; i < 100; i++) {
-            for(let j = 0; j < 80; j++) {
-                points.push({ location: createGeocodedPoint(i, j)})
+
+        /// Step of `0.01` generates ~10 per one mile radius in the center of the box. Steps of `0.003` or smaller can cause performance issues due to the order of growth of points
+        const step = 0.01
+        for(let longitude = westLong; longitude < eastLong; longitude += step) {
+            for(let latitude = southLat; latitude < northLat; latitude += step) {
+                points.push({ location: createGeocodedPoint(longitude, latitude)})
             }
         }
+        console.log(`Generated ${points.length} points`)
+
+
+        /// Bypass setting logic due to large size of insert
         await geocodeCollection.insertMany(points);   
 
     } catch (err) {
@@ -41,6 +55,7 @@ async function seedDB() {
         const db = await mongoConnection();
         await db.closeConnection();
         console.log("Connection closed!");
+        process.exit(0);
     }
 }
 
