5b5d36f1ac7711cce6a16de53cbf38ce0365a47a
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2021-11-30 21:01:54 -0500

    Implemented intial geocoded location logic (#11)
    Overall Changes
    ---
    - Created MongoDB integration
       - This includes the `MongoDB` package
       - `apollo-datasource-mongodb` as it is Apollo Server's recommended Mongo integration
       - Boiler plate for Mongo from CS-546
       - Geocoded locations uses a 2d sphere object. 
    
    - Create a seed file for the data base
      - Data is seeded around the NJ area with ~10 cars per mile radius
      - Added `npm run seed` to run the file
      - File only has minimal error handling
      
    - Server start-up now supports `async` data source creation
    - Created inheritance tree for `GeoJSON` types to perform validation and type mapping
    
    
    GraphQL Schema changes
      --- 
    - Placeholder point objects created
    - Created resolvers for queries and mutations
      - Query provides point in DB to allow testing of front-end integrations
      - Mutation allows for adhoc submissions of points and testing of validation logic
    - Created `GeocodedLocationDataSource` to support example logic for querying locations

---
+++ server/src/config/mongoCollections.js
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Base provided from lecture notes of CS546.
+ ******************************************************************************/
+ const dbConnection = require("./mongoConnection");
+
+ /* This will allow you to have one reference to each collection per app */
+ /* Feel free to copy and paste this this */
+ const getCollectionFn = (collection, indexCreationObject) => {
+     let _col = undefined;
+ 
+     return async () => {
+         if (!_col) {
+             const db = await dbConnection();
+             _col = await db.collection(collection);
+
+             if(indexCreationObject && typeof indexCreationObject === 'object') {
+                await _col.createIndex( indexCreationObject)
+             }
+         }
+ 
+         return _col;
+     };
+ };
+ 
+ /* Now, you can list your collections here: */
+ module.exports = {
+    geocode: getCollectionFn("geocode", { location : "2dsphere" } )
+ };
+ 
