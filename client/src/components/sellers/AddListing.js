c9132e295ff3d2188d93b58e0f5f8beb394e70cb
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2021-12-13 21:26:53 -0500

    Add labels to forms
    - Add listing has forms and action elements
    - MiniVehicleForm has a hidden label

--- client/src/components/sellers/AddListing.js
+++ client/src/components/sellers/AddListing.js
@@ -44,29 +44,110 @@ const AddListing = () => {
       <Sidebar side_links={slinks} />
       <div className="mainbody">
         <h1>Add a Listing</h1>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+        <br />
+        <br />
+        <br />
+        <br />
         <form onSubmit={submitHandler}>
           <div className="form-group flex-col">
-            <input id="vin" type="text" name="vin" placeholder="Vin..." required />
+            <label>
+              Vin:
+              <input
+                id="vin"
+                type="text"
+                name="vin"
+                placeholder="Vin..."
+                className="mx-2"
+                title="The full VIN of the vehicle"
+                required
+              />
+            </label>
+
             <div id="coordinates" className="form-group flex-row">
-              <input id="longitude" type="number" name="longitude" placeholder="Longitude..." step="0.0000001" min="-180" max="180" required />
-              <input id="latitude" type="number" name="latitude" placeholder="Latitude..." step="0.0000001" min="-90" max="90" required />
+              <label>
+                Longitude
+                <input
+                  id="longitude"
+                  type="number"
+                  name="longitude"
+                  placeholder="Longitude..."
+                  step="0.0000001"
+                  min="-180"
+                  max="180"
+                  className="mx-2"
+                  title="The longitude of where the vehicle is sold"
+                  required
+                />
+              </label>
+              <label>
+                Latitude
+                <input
+                  id="latitude"
+                  type="number"
+                  name="latitude"
+                  placeholder="Latitude..."
+                  step="0.0000001"
+                  min="-90"
+                  max="90"
+                  className="mx-2"
+                  title="The longitude of where the vehicle is sold"
+                  required
+                />
+              </label>
             </div>
-            <input id="price" type="number" name="price" placeholder="Price..." required />
-            <input id="millage" type="number" name="millage" placeholder="Millage..." required />
-            <input id="exteriorColor" type="text" name="exteriorColor" placeholder="Exterior Color..." required />
-            <input id="interiorColor" type="text" name="interiorColor" placeholder="Interior Color..." required />
+            <label>
+              Price
+              <input
+                id="price"
+                type="number"
+                name="price"
+                placeholder="Price..."
+                className="mx-2"
+                title="The list price to show"
+                required
+              />
+            </label>
+            <label>
+              Millage
+              <input
+                id="millage"
+                type="number"
+                name="millage"
+                placeholder="Millage..."
+                className="mx-2"
+                title="The millage on the odometer"
+                required
+              />
+            </label>
+            <label>
+              Exterior Color
+              <input
+                id="exteriorColor"
+                type="text"
+                name="exteriorColor"
+                placeholder="Exterior Color..."
+                className="mx-2"
+                title="The primary exterior color"
+                required
+              />
+            </label>
+            <label>
+              Interior Color
+              <input
+                id="interiorColor"
+                type="text"
+                name="interiorColor"
+                placeholder="Interior Color..."
+                className="mx-2"
+                title="The primary interior color"
+                required
+              />
+            </label>
+
             {/* <input id="photos" type="text" name="photos" placeholder="Photos..." required /> */}
           </div>
-          
-          <button
-            className="btn-primary px-10"
-            type="submit"
-            name="submitBtn"
-          >
+
+          <button className="btn-primary px-10" type="submit" name="submitBtn">
             Add Listing
           </button>
           {/* vin: Required, full vin number. Must not currently have a listing
@@ -78,9 +159,9 @@ const AddListing = () => {
           photos: Array, currently ignored */}
         </form>
         <div className="errors">
-          { errors.map(error => (
-              <p className="error">{error}</p>
-            )) }
+          {errors.map((error) => (
+            <p className="error">{error}</p>
+          ))}
         </div>
         {createdListing ? (
           <div>
