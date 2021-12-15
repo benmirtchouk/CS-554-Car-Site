2653a6c5d825211ca66921d47fb24fae6982533c
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2021-12-15 03:21:35 -0500

    Added validation to image fetching

--- server/routes/images.js
+++ server/routes/images.js
@@ -3,16 +3,20 @@ const router = express.Router();
 const { listingImages } = require('../src/config/mongoFileStream');
 
 router.get("/:name", async (req, res) => {
-    /// TODO validate 
     const name = req.params.name;
+    if(name.length === 0) { 
+        return res.json("400").message({ message: "Name cannot be blank"})
+    }
     const bucket = await listingImages();
 
-    /// TODO set correctly
-    res.contentType = ('image/jpg');
-
-    /// TODO error handling
-    bucket.openDownloadStreamByName(name).pipe(res);
-
+    return bucket.openDownloadStreamByName(name)
+    .on("error", (e) => {
+        if(e.code !== 'ENOENT') {
+            console.error(e);
+        }
+        return res.status(404).json({message: "Not found"});
+    })
+    .pipe(res);
 
 
 })
