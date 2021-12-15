afbd4b57a62a044d7cc443267dbd5743dc43f01a
daharrington60 <dharring@stevens.edu>
2021-12-15 17:20:37 -0500

    updating seed data

--- server/src/tasks/seed.js
+++ server/src/tasks/seed.js
@@ -1,9 +1,12 @@
-const { geocode, listings } = require("../config/mongoCollections");
+const { geocode, listings, accounts } = require("../config/mongoCollections");
 const mongoConnection = require("../config/mongoConnection");
 const { ObjectId } = require("mongodb");
 const listingData = require('./listingSeed.json');
+const accountData = require('./accountSeed.json');
 const listingMongoOperation = require('../MongoOperations/listing');
+const accountMongoOperation = require('../MongoOperations/account');
 const VehicleListing = require('../DataModel/Automotive/VehicleListing');
+const Account = require('../DataModel/Account/Account');
 
 // #MARK:- Data construction operations, does *not* do validation, that is handled in data functions (or programmer validation when bypassed)
 
@@ -19,18 +22,31 @@ const createGeocodedPoint = (longitude, latitude) => {
 
 async function seedDB() {
 
-
     console.log("Creating connections to collections");
     const listingCollection = await listings();
+    const accountCollection = await accounts();
 
     console.log("Dropping any previous databases")
     await listingCollection.deleteMany({});
+    await accountCollection.deleteMany({});
 
     try {
         console.log("Inserting Listings")
         for(const listingRawData of listingData) {
             const listing = new VehicleListing(listingRawData);
             await listingMongoOperation.insertListing(listing);
+        }
+
+    } catch (err) {
+        console.error(err);
+    } 
+     
+
+    try {
+        console.log("Inserting Account Information")
+        for(const accountRawData of accountData) {
+            const account = new Account(accountRawData);
+            await accountMongoOperation.createAccount(account);
         }
 
     } catch (err) {
