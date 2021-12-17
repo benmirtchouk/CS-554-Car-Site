a5eed79798abcb4b622f8335fdddd686f5bb76ea
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2021-12-17 03:02:08 -0500

    Correct link text for tota11y

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -30,8 +30,9 @@ const Home = ({ setSidebar }) => {
         </section>
         <h2> Getting Started</h2>
         To look at vehicles for sale, there is a search bar at the top of page
-        to allow easy access. If you are interested in selling a vehicle you can{" "}
-        <Link to="add_listing"> go here</Link> to list a vehicle for sale.
+        to allow easy access. If you are interested in{" "}
+        <Link to="add_listing">selling a vehicle you can go here</Link> to list
+        a vehicle for sale.
       </main>
     </>
   );
