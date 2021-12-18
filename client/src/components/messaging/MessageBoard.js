364f82194607ba59b086787b3930b20f1d70d6b2
Ben Mirtchouk <benmirtchouk@gmail.com>
2021-12-18 16:32:28 -0500

    messageboard styles

--- client/src/components/messaging/MessageBoard.js
+++ client/src/components/messaging/MessageBoard.js
@@ -5,6 +5,7 @@ import { AuthContext } from "../../firebase/Auth";
 const MessageBoard = (props) => {
   const [ chat, setChat ] = useState([]);
   const chatRef = useRef([]);
+  const chatEndRef = useRef(null)
   
   const { currentUser } = useContext(AuthContext);
   const { socket } = props;
@@ -12,20 +13,28 @@ const MessageBoard = (props) => {
   const current_username = currentUser.displayName ?? currentUser.email;
   const uid = currentUser.uid;
 
-  console.log('cur', currentUser);
-  console.log('socket', socket);
-
   useEffect(() => {
     chatRef.current = chat;
   }, [chat]);
 
+  const add_chat = (username, message) => {
+    // We cannot just update the ref to 'chat' because changes to it will not trigger a re-render
+    // Thus we change the state 'chat' and later (above useEffect) waterfall this change to the ref
+    const time = new Date().toLocaleTimeString();
+    setChat([ ...chatRef.current, { time, username, message } ]);
+  };
+
+  const scrollToBottom = () => {
+    if (chatEndRef.current) {
+      chatEndRef.current.scrollTop = chatEndRef.current.scrollHeight;
+    }
+  }
+
   useEffect(() => {
-    const add_chat = (username, message) => {
-      // We cannot just update the ref to 'chat' because changes to it will not trigger a re-render
-      // Thus we change the state 'chat' and later (above useEffect) waterfall this change to the ref
-      setChat([ ...chatRef.current, { username, message } ]);
-    };
-    
+    scrollToBottom();
+  }, [chat]);
+
+  useEffect(() => {
     socket.emit('user_join_leave', { uid, username: current_username, joined: true } );
 
     socket.on('message', ({ username, message }) => {
@@ -40,51 +49,42 @@ const MessageBoard = (props) => {
     }
   }, [socket, uid]);
 
-  // useEffect(() => {
-  //   socketRef.current.on('message', (message) => {
-  //     console.log(message);
-  //   });
-  // })
-
-
   const onMessageSubmit = (e) => {
     e.preventDefault();
     const msgEle = document.getElementById('message');
-    console.log(`${msgEle.value} received`);
     socket.emit('message', { uid, username: current_username, message: msgEle.value });
+    add_chat(current_username, msgEle.value);
     msgEle.value = '';
     msgEle.focus();
   }
 
-
   return (
     <div className="main_layout">
-      <div className="mainbody">
+      <div className="main-chat-body">
         <h1>MessageBoard</h1>
         <br />
         <br />
-
-        <p>User: {current_username}</p>
-        <div id="chatlog">
-          { chat.map(({ username, message}, index) => (
-            <div key={index}>
-              <h3>
-                {username}: <span>{message}</span>
-              </h3>
-            </div>
-          )) }
-        </div>
-        <form onSubmit={onMessageSubmit}>
-          <div>
+        
+        <div className="chat-container">
+          <div className="chatlog" ref={chatEndRef}>
+            { chat.map(({ time, username, message}, index) => (
+              <div key={index}>
+                <p>
+                  {`${time}: <${username}> `}<span>{message}</span>
+                </p>
+              </div>
+            )) }
+          </div>
+          <form onSubmit={onMessageSubmit}>
             <input
               name="message"
               id="message"
               variant="outlined"
               label="Message"
             />
-          </div>
-          <button type="submit" id="send-message">Send Message</button>
-        </form>
+            <button type="submit" id="send-message">Send Message</button>
+          </form>
+        </div>
       </div>
     </div>
   );
